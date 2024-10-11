6ebc288ccefb50a79dbb687ad7b5a680180e5017
Reder <maxsimgolodryga2007@gmail.com>
2024-10-11 12:48:03 +0500

    TEyr

--- test.py
+++ test.py
@@ -1,21 +1,18 @@
 import psycopg2
 from flask import Flask, render_template, request
 app = Flask(__name__)
-def grt():
-    conn=psycopg2.connect(
+conn=psycopg2.connect(
                       host='localhost',
                       user='tert',
                       password='Teratra14',
-                      dbname='ver',
+                      dbname='postgres',
                       )
-    return conn
 
 @app.route('/', methods=['GET','POST'])
 def hello():
     automobile=[]
     
     if request.method=='POST':
-        conn=grt()
         cur=conn.cursor()
         
         harp=request.form['harp']
@@ -30,5 +27,5 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0', port=80)
 
