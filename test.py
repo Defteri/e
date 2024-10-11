4cb58e656cc37116468a9171ba8bb05e12710ef5
Reder <maxsimgolodryga2007@gmail.com>
2024-10-11 12:56:04 +0500

    ttrt

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import psycopg2
 from flask import Flask, render_template, request
 app = Flask(__name__)
-conn=psycopg2.connect(
+
+@app.route('/', methods=['GET','POST'])
+def hello():
+    automobile=[]
+
+    conn=psycopg2.connect(
                       host='localhost',
                       user='tert',
                       password='Teratra14',
                       dbname='postgres',
                       )
-
-@app.route('/', methods=['GET','POST'])
-def hello():
-    automobile=[]
     
     if request.method=='POST':
         cur=conn.cursor()
